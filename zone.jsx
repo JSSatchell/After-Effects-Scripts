449788d94ad535a39344f31778a39ab5bd7306c4
JSSatchell <99512204+JSSatchell@users.noreply.github.com>
2023-02-15 14:31:18 -0600

    Update and rename zing.jsx to zone.jsx

--- zone.jsx
+++ zone.jsx
@@ -1,6 +1,6 @@
 // JSSatchell 2023
 
-// zing is my version of creating a new Solid, or Battle Axe's Volume
+// zone is my version of creating a new Solid, or Battle Axe's Volume
 
 // Shamelessly inspired by Battle Axe's awesome Void scripts which can be found here: https://www.battleaxe.co/void
 // Really I just missed the feature of Turbo Layers to create Solids and Adjustment Layers that were the same length as the selected layers, so I took it as a scripting challenge to build my own toolkit
@@ -9,21 +9,21 @@ var comp = app.project.activeItem;
 var layers = comp.selectedLayers;
 var allLayers = comp.layers;
 
-app.beginUndoGroup("Add zing");
+app.beginUndoGroup("Add zone");
 
 index=1;
 for (var i=1; i < allLayers.length; i++) {
-    if (allLayers[i].name.search("zing")>0)
+    if (allLayers[i].name.search("zone")>0)
         index++;
 }
-var zingLayer = comp.layers.addShape();
-zingLayer.name = " - zing " + index + " - ";
-var zingGroup = zingLayer.property("Contents").addProperty("ADBE Vector Group");
-zingGroup.name = "zing";
-var zingShape = zingGroup.property("Contents").addProperty("ADBE Vector Shape - Rect");
-zingShape.property("Size").setValue([comp.width,comp.height]);
-var zingFill = zingGroup.property("Contents").addProperty("ADBE Vector Graphic - Fill");
-zingFill.property("Color").setValue([0,0,0]);
+var zoneLayer = comp.layers.addShape();
+zoneLayer.name = " - zone " + index + " - ";
+var zoneGroup = zoneLayer.property("Contents").addProperty("ADBE Vector Group");
+zoneGroup.name = "zone";
+var zoneShape = zoneGroup.property("Contents").addProperty("ADBE Vector Shape - Rect");
+zoneShape.property("Size").setValue([comp.width,comp.height]);
+var zoneFill = zoneGroup.property("Contents").addProperty("ADBE Vector Graphic - Fill");
+zoneFill.property("Color").setValue([0,0,0]);
 if (app.preferences.havePref("Label Preference Indices Section 5", "Solid Label Index", PREFType.PREF_Type_MACHINE_INDEPENDENT) == 1) {
     var solidColor = app.preferences.getPrefAsLong("Label Preference Indices Section 5", "Solid Label Index", PREFType.PREF_Type_MACHINE_INDEPENDENT);
 } else if (app.preferences.havePref("Label Preference Indices Section 5", "Solid Label Index 2", PREFType.PREF_Type_MACHINE_INDEPENDENT) == 1) {
@@ -34,12 +34,12 @@ if (app.preferences.havePref("Label Preference Indices Section 5", "Solid Label 
 
 if(layers.length > 0) {
     var newIn;
-    var minIn = zingLayer.outPoint;
+    var minIn = zoneLayer.outPoint;
     var newOut;
     var maxOut = 0;
     var topIndx = layers.length;
     var newIndx = layers.length;
-    zingLayer.moveToEnd();
+    zoneLayer.moveToEnd();
     
     for(var i = 0; i < layers.length; i++){
         newIn = layers[i].inPoint;
@@ -51,14 +51,14 @@ if(layers.length > 0) {
         if(newOut > maxOut) {
             maxOut = newOut;
         }
-        if(newIndx < zingLayer.index) {
-            zingLayer.moveBefore(layers[i]);
+        if(newIndx < zoneLayer.index) {
+            zoneLayer.moveBefore(layers[i]);
         }
     }
-    zingLayer.inPoint = minIn;
-    zingLayer.outPoint = maxOut;
+    zoneLayer.inPoint = minIn;
+    zoneLayer.outPoint = maxOut;
 }
 
-zingLayer.label = solidColor;
+zoneLayer.label = solidColor;
 
 app.endUndoGroup();
