4114ef09609b24b8e2ffe8bb9e611b95a0f279e1
JSSatchell <99512204+JSSatchell@users.noreply.github.com>
2023-02-24 11:36:13 -0600

    Create Plugins_KBar_Markers.jsx

---
+++ Plugins_KBar_Markers.jsx
@@ -0,0 +1,59 @@
+// JSSatchell 2023
+
+// For use with KBar3 which can be found here: https://aescripts.com/kbar/
+// Pass an argument of 1-16 to add a marker of a specific color, all other values will create a marker of the default color.
+
+var comp = app.project.activeItem;
+var layers = comp.selectedLayers;
+var button = (typeof kbar !== 'undefined') ? kbar.button : null;
+
+if (button) {
+    try {
+        var argNum = parseInt(button.argument);
+        if (argNum <= 16) {
+            addColorMarker(argNum);
+        } else {
+            addDefaultMarker();
+        }
+    } catch (error) {
+        addDefaultMarker();
+    }
+} else {
+    Window.alert("Please use with KBar.");
+}
+
+function addColorMarker(colVal) {
+    app.beginUndoGroup ("Add Marker");
+    var comp = app.project.activeItem;
+    var layers = comp.selectedLayers;
+    var m = new MarkerValue("");
+    m.label = colVal;
+    if (layers.length > 0) {
+        for(var i = 0; i < layers.length; i++) {
+            var t = layers[i].time;
+            layers[i].property("Marker").setValueAtTime(t, m);
+        }
+    } else {
+        var t = comp.time;
+        comp.markerProperty.setValueAtTime(t, m);
+    }
+    app.endUndoGroup ();
+}
+
+function addDefaultMarker() {
+    app.beginUndoGroup ("Add Marker");
+    var comp = app.project.activeItem;
+    var layers = comp.selectedLayers;
+    var m = new MarkerValue("");
+    // m.label = button.argument;
+    if (layers.length > 0) {
+        for(var i = 0; i < layers.length; i++) {
+            var t = layers[i].time;
+            layers[i].property("Marker").setValueAtTime(t, m);
+        }
+    } else {
+        var t = comp.time;
+        comp.markerProperty.setValueAtTime(t, m);
+    }
+    app.endUndoGroup ();
+}
