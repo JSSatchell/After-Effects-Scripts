8b593ddfc67a293cba08f776ffd2f0c0ce29ded5
JSSatchell <99512204+JSSatchell@users.noreply.github.com>
2023-03-30 15:57:04 -0500

    Update Increment Prefix.jsx
    Combined KBar and UI panel.

--- Increment Prefix.jsx
+++ Increment Prefix.jsx
@@ -2,48 +2,87 @@
 
 // Adds or subtracts one from numbered layer names
 
-// Should be used only in the composition timeline window with one or more layers selected which are named starting with a two-digit number followed by an underscore.
-// If the layer name begins with a number less than 10, it must start with a leading 0 which will be retained when using the script.
+// KBar arguments:
+//   - increment
+//   - decrement
 
 // Demonstrated in this video: https://youtu.be/o37TgQ9jIYg
 
-var window = new Window("palette", "Increment Index", undefined);
-var increase = window.add("button",undefined,"Increase");
-var decrease = window.add("button",undefined,"Decrease");
-window.show();
+var kButton = (typeof kbar !== 'undefined') ? kbar.button : null;
 
-increase.onClick = function() {
+if (kButton) {
+    switch(kButton.argument) {
+        case 'increment':
+            app.beginUndoGroup('Increment Prefix');
+            increment();
+            app.endUndoGroup();
+        break;
+
+        case 'decrement':
+            app.beginUndoGroup('Decrement Prefix');
+            decrement();
+            app.endUndoGroup();
+        break;
+
+        case '':
+            buildPanel();
+        break;
+    
+        default:
+            Window.alert("Argument " + kButton.argument + " is invalid.");
+        break;
+    }
+} else {
+    buildPanel();
+}
+
+function buildPanel() {
+    var window = new Window("palette", "Increment Index", undefined);
+    var increase = window.add("button",undefined,"Increase");
+    var decrease = window.add("button",undefined,"Decrease");
+    window.show();
+
+    increase.onClick = function() {
         var comp = app.project.activeItem;
-        
         var layers = comp.selectedLayers;
-        
-        for(var i = 0; i < layers.length; i++) {
-                var prefix = layers[i].name.split("_")[0];
-                var namePostPrefix = layers[i].name.substring(2);
-                var newPrefix = parseInt(prefix, 10) + 1;
-                if (newPrefix < 10) {
-                    layers[i].name = "0" + newPrefix + namePostPrefix;
-                    } else {
-                        layers[i].name = newPrefix + namePostPrefix;
-                        }
-            }
+        app.beginUndoGroup('Increment Prefix');
+        increment();
+        app.endUndoGroup();
     }
 
-decrease.onClick = function() {
+    decrease.onClick = function() {
         var comp = app.project.activeItem;
-        
         var layers = comp.selectedLayers;
-        
-        for(var i = 0; i < layers.length; i++) {
-                var prefix = layers[i].name.split("_")[0];
-                var namePostPrefix = layers[i].name.substring(2);
-                var newPrefix = parseInt(prefix, 10) - 1;
-                if (newPrefix < 1) {
-                    layers[i].name = prefix + namePostPrefix;
-                    } else if (newPrefix < 10) {
-                        layers[i].name = "0" + newPrefix + namePostPrefix;
-                        } else {
-                            layers[i].name = newPrefix + namePostPrefix;
-                            }
-            }
+        app.beginUndoGroup('Decrement Prefix');
+        decrement();
+        app.endUndoGroup();
     }
+}
+
+function increment() {
+    for(var i = 0; i < layers.length; i++) {
+        var prefix = layers[i].name.split("_")[0];
+        var namePostPrefix = layers[i].name.substring(2);
+        var newPrefix = parseInt(prefix, 10) + 1;
+        if (newPrefix < 10) {
+            layers[i].name = "0" + newPrefix + namePostPrefix;
+            } else {
+                layers[i].name = newPrefix + namePostPrefix;
+                }
+    }
+}
+
+function decrement() {
+    for(var i = 0; i < layers.length; i++) {
+        var prefix = layers[i].name.split("_")[0];
+        var namePostPrefix = layers[i].name.substring(2);
+        var newPrefix = parseInt(prefix, 10) - 1;
+        if (newPrefix < 1) {
+            layers[i].name = prefix + namePostPrefix;
+            } else if (newPrefix < 10) {
+                layers[i].name = "0" + newPrefix + namePostPrefix;
+                } else {
+                    layers[i].name = newPrefix + namePostPrefix;
+                    }
+    }
+}
