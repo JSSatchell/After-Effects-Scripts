6d8243be819a6ea5aed29b06fa8a679deeb64f0b
JSSatchell <99512204+JSSatchell@users.noreply.github.com>
2023-04-04 14:23:38 -0500

    Update Add Handles.jsx

--- Add Handles.jsx
+++ Add Handles.jsx
@@ -2,28 +2,35 @@
 
 // Adds 1 second to the beginning and end of selected layers, or number passed by user via KBar
 
+var handle = 1;
 var button = (typeof kbar !== 'undefined') ? kbar.button : null;
 
 if (button) {
+   app.beginUndoGroup("Add Handles");
    try {
-      var argNum = parseInt(button.argument);
+      var argNum = parseFloat(button.argument);
       addHandles(argNum);
    } catch (error) {
-      addHandles(1);
+      addHandles(handle);
    }
+   app.endUndoGroup();
 } else {
-   addHandles(1);
+   app.beginUndoGroup("Add Handles");
+   addHandles(handle);
+   app.endUndoGroup();
 }
 
 function addHandles(addTime) {
    var comp = app.project.activeItem;
    var layers = comp.selectedLayers;
-   app.beginUndoGroup("Add Handles");
    for(var i = 0; i < layers.length; i++) {
+      if (layers[i].inPoint>layers[i].outPoint) // Check for reversed layers
+         addTime *= -1;
+
       newIn = layers[i].inPoint - addTime;
       newOut = layers[i].outPoint + addTime;
+
       layers[i].inPoint = newIn;
       layers[i].outPoint = newOut;
    }
-   app.endUndoGroup();
 }
